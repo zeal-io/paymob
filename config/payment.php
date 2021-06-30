35020ecf1a2f0eb80dc5b55d8b1dc31106063a41
ahmdalgendi <ahmd.algendi@gmail.com>
2021-07-01 00:11:23 +0200

    move type casting to the paymob class

--- config/payment.php
+++ config/payment.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paymob' => [
-        'api_key' => env('PAYMOB_API_KEY', ''),
-        'zeal_auth_integration' => (int) env('PAYMOB_AUTH_INTEGRATION', ''),
-        'zeal_payment_integration' => (int) env('PAYMOB_PAYMENT_INTEGRATION', ''),
-    ],
+		'paymob' => [
+				'api_key' => env('PAYMOB_API_KEY', ''),
+				'zeal_auth_integration' => env('PAYMOB_AUTH_INTEGRATION', ''),
+				'zeal_payment_integration' => env('PAYMOB_PAYMENT_INTEGRATION', ''),
+		],
 ];
