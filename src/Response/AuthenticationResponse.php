7e1db455ddd951f085a955b15442280a3b8ed9be
Omar <46730385+OmarAbdelazizMahmed@users.noreply.github.com>
2025-06-15 19:14:16 +0300

    Log authentication response

--- src/Response/AuthenticationResponse.php
+++ src/Response/AuthenticationResponse.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Zeal\Paymob\Response;
 
+use App\Models\Log;
 use Zeal\Paymob\Exceptions\InvalidAuthenticationException;
 use Illuminate\Http\Client\Response;
 
@@ -29,7 +30,12 @@ final class AuthenticationResponse
     {
         $this->response = $response;
 
-        $this->body = (object)json_decode((string) $response->getBody());
+        $this->body = (object) json_decode((string) $response->getBody());
+
+        Log::create([
+            'key' => 'paymob_authentication_response',
+            'payload' => json_encode($this->body),
+        ]);
 
         $this->handleResponseExceptions();
     }
